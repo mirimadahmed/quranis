6e25517ed11dcec18d0078273ba2e71b2c1eb2c6
Mir Imad Ahmed <mirimadahmed@outlook.com>
2021-01-28 13:32:28 +0500

    index of quran

---
+++ src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row text-left">
+    <div v-for="surah in surahs" :key="surah.number" class="col-2">
+      {{ surah.number }}. {{ surah.nameEnglish }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    surahs: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
